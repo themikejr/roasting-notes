8791193850cfa886c8761b8241d1f590f2b3b962
Mike Brinker <themikejr@gmail.com>
2018-02-09 17:44:38 -0500

    another shot at postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180209164029) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "profiles", force: :cascade do |t|
     t.string "coffee_name"
     t.string "origin_country"
