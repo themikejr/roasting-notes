68f3dbaf5ebee00d6302630c5469f9d6f8e54876
Mike Brinker <themikejr@gmail.com>
2018-02-09 16:22:08 -0500

    Show list of profiles.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,10 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @profiles = Profile.all
+  end
+
   def show
     @profile = Profile.find(params[:id])
   end
